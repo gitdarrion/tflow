64b977c50410d45d5c05d9740215876ff9225d73
Darrion Dion Banks <darrion@darrionmac.local>
2017-08-08 00:20:30 -0400

    Executes add operation on tensors.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 """ Prints object information. """
 node1 = tf.constant(3, dtype=tf.float32)
 node2 = tf.constant(4.0)
@@ -8,3 +10,8 @@ print(node1, node2)
 """ Prints tensor values. """
 session = tf.Session()
 print(session.run([node1, node2]))
+
+""" Operations. """
+node3 = tf.add(node1, node2)
+print("node3: ", node3)
+print("session.run(node3): ", session.run(node3))
