e76872c31348ce2710e673866de112669cf8dc0b
Darrion Dion Banks <darrion@darrionmac.local>
2017-08-09 16:49:18 -0400

    Executes add operation on tensors using placeholders and operation node.

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ session = tf.Session()
 print(session.run([node1, node2]))
 
 """ Operations. """
+# Operations with TF functions.
 node3 = tf.add(node1, node2)
 print("node3: ", node3)
 print("session.run(node3): ", session.run(node3))
+
+# Operations with placeholders.
+a = tf.placeholder(dtype=tf.float32)
+b = tf.placeholder(dtype=tf.float32)
+adder_node = a + b
+print(session.run(adder_node, {a: 3, b: 4}))
+print(session.run(adder_node, {a: [1,3], b: [2,4]}))
