0a13396ce897bd0242d1750cbed28c3bd835e9be
Darrion Dion Banks <darrion@darrionmac.local>
2017-08-16 22:20:49 -0400

    Creates variables, initializes variables, and creates linear model.

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import os
 import tensorflow as tf
 
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
 
-""" Prints object information. """
+""" Print object information. """
+
 node1 = tf.constant(3, dtype=tf.float32)
 node2 = tf.constant(4.0)
 print(node1, node2)
 
-""" Prints tensor values. """
+""" Print tensor values. """
+
 session = tf.Session()
 print(session.run([node1, node2]))
 
-""" Operations. """
-# Operations with TF functions.
+""" Execute operations on tensors. """
+
+### FUNCTIONS ###
 node3 = tf.add(node1, node2)
 print("node3: ", node3)
 print("session.run(node3): ", session.run(node3))
 
-# Operations with placeholders.
+### PLACEHOLDERS ###
 a = tf.placeholder(dtype=tf.float32)
 b = tf.placeholder(dtype=tf.float32)
 adder_node = a + b
 print(session.run(adder_node, {a: 3, b: 4}))
 print(session.run(adder_node, {a: [1,3], b: [2,4]}))
+add_and_triple = adder_node * 3
+print(session.run(add_and_triple, {a: 3, b: 4.5}))
+
+### VARIABLES ###
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(dtype=tf.float32)
+linear_model = W * x + b
+init = tf.global_variables_initializer()
+session.run(init)
+print(session.run(linear_model, {x:[1,2,3,4]}))
