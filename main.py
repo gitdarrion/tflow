17dd4d20108b37cc830b94608e877d56ec5eae1d
Darrion Dion Banks <darrion@darrionmac.local>
2017-08-16 23:24:40 -0400

    Uses tf.square and tf.reduce_sum to calculate model loss.

--- main.py
+++ main.py
@@ -39,3 +39,9 @@ linear_model = W * x + b
 init = tf.global_variables_initializer()
 session.run(init)
 print(session.run(linear_model, {x:[1,2,3,4]}))
+
+### REDUCE SUM / ERROR ###
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(session.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
